f04fc7fd0e4fd93dad1445f2b5d7edcf23844956
Jared Mackey <jmackey@perfectpitchtech.com>
2017-01-20 18:28:43 -0700

    updated setup

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
+
 from django_asyncio_redis import __version__
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
 
 setup(
     name="django-asyncio-redis",
@@ -7,13 +13,12 @@ setup(
     author_email='jared@mackey.tech',
     version=__version__,
     description="A asyncio-redis backend for Django!",
+    long_description=readme(),
+    keywords='django asyncio redis cache',
     url='https://github.com/mackeyja92/django-asyncio-redis',
     license='MIT License',
     packages=[
         'django_asyncio_redis'
     ],
-    install_requirements=[
-        'django>=1.8',
-        'asyncio-redis'
-    ]
+    install_requires=['django>=1.8', 'asyncio-redis']
 )
